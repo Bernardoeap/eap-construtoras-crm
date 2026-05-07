db0dabc2a4c3e9a5f60158b5d00f5f4bdd35e198
Bernardoeap <bernardo@engenhariaeperformance.com>
2026-05-07 10:28:02 -0300

    fix: marca libsql e Prisma como serverExternalPackages (webpack nao consegue bundlear)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  serverExternalPackages: [
+    "@prisma/client",
+    "@prisma/adapter-libsql",
+    "@libsql/client",
+    "@libsql/isomorphic-ws",
+    "@libsql/isomorphic-fetch",
+    "libsql",
+  ],
+  outputFileTracingRoot: __dirname,
+};
 
 module.exports = nextConfig;
